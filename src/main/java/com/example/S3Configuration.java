1496f40f883929616af471ddb0e3da2da17bdc97
Lucas <lucasvalenteds@gmail.com>
2021-07-24 11:40:35 -0300

    Configuring S3

---
+++ src/main/java/com/example/S3Configuration.java
@@ -0,0 +1,47 @@
+package com.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+import java.net.URI;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class S3Configuration {
+
+    @Bean
+    URI endpoint(Environment environment) {
+        return URI.create(environment.getRequiredProperty("aws.url", String.class));
+    }
+
+    @Bean
+    Region region(Environment environment) {
+        return Region.of(environment.getRequiredProperty("aws.region", String.class));
+    }
+
+    @Bean("bucket")
+    String bucket(Environment environment) {
+        return environment.getRequiredProperty("aws.s3.bucket", String.class);
+    }
+
+    @Bean
+    S3AsyncClient client(URI endpoint, Region region) {
+        return S3AsyncClient.builder()
+                .endpointOverride(endpoint)
+                .region(region)
+                .build();
+    }
+
+    @Bean
+    S3Presigner signer(URI endpoint, Region region) {
+        return S3Presigner.builder()
+                .endpointOverride(endpoint)
+                .region(region)
+                .build();
+    }
+}
