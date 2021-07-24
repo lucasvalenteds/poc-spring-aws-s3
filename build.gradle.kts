d2f30c9e1dd6870b60a4fa2cd407687505a6ba6d
Lucas <lucasvalenteds@gmail.com>
2021-07-24 11:39:29 -0300

    Adding application dependencies
    Spring Framework, Reactor, Jackson and AWS SDK.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,19 @@ dependencies {
     implementation("org.apache.logging.log4j", "log4j-core", properties["version.log4j"].toString())
     implementation("org.slf4j", "slf4j-simple", properties["version.slf4j"].toString())
 
+    implementation("org.springframework", "spring-core", properties["version.spring"].toString())
+    implementation("org.springframework", "spring-context", properties["version.spring"].toString())
+    implementation("org.springframework", "spring-webflux", properties["version.spring"].toString())
+    testImplementation("org.springframework", "spring-test", properties["version.spring"].toString())
+
+    implementation("io.projectreactor", "reactor-core", properties["version.reactor"].toString())
+    testImplementation("io.projectreactor", "reactor-test", properties["version.reactor"].toString())
+
+    implementation("com.fasterxml.jackson.core", "jackson-databind", properties["version.jackson"].toString())
+    implementation("com.fasterxml.jackson.datatype", "jackson-datatype-jsr310", properties["version.jackson"].toString())
+
+    implementation("software.amazon.awssdk", "s3", properties["version.aws"].toString())
+
     testImplementation("org.junit.jupiter", "junit-jupiter", properties["version.junit"].toString())
 }
 
