fb906dbfb8ae8e4f0c08a23631ffcfe3383fbb82
Lucas <lucasvalenteds@gmail.com>
2021-07-25 12:10:08 -0300

    Creating uploads directory on service startup

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,8 +5,12 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
 import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.netty.http.server.HttpServer;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Duration;
 
 public final class Main {
@@ -17,6 +21,13 @@ public final class Main {
         var context = new AnnotationConfigApplicationContext(Main.class.getPackageName());
 
         var environment = context.getEnvironment();
+
+        Mono.just(context.getBean("uploads", Path.class))
+            .filterWhen(path -> Mono.fromCallable(() -> !Files.exists(path)))
+            .flatMap(path -> Mono.fromCallable(() -> Files.createDirectory(path)))
+            .doOnNext(path -> LOGGER.info("Directory created: {}", path))
+            .subscribeOn(Schedulers.boundedElastic())
+            .subscribe();
 
         HttpServer.create()
             .port(environment.getRequiredProperty("server.port", Integer.class))
