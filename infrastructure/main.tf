b0296429bb60613878dd9e590c1eac775fb3dcc7
Lucas <lucasvalenteds@gmail.com>
2021-07-25 12:23:54 -0300

    Provisioning S3 on AWS using Terraform

---
+++ infrastructure/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.51.0"
+    }
+  }
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "bucket_name" {
+    default = "example-company-s3-${var.region}"
+}
+
+provider "aws" {
+    access_key = var.access_key
+    secret_key = var.secret_key
+    region = var.region
+}
+
+resource "aws_s3_bucket" "poc_spring_aws_s3_bucket" {
+    bucket = var.bucket_name
+    acl = "public-read-write"
+}
+
